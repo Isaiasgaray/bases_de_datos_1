196aa70ad36b197b3e2b469dc8672e4df1734966
Isaías <isaiasgaraygorta@gmail.com>
2022-10-26 18:23:22 -0300

    ~

---
+++ tp1/scripts/creacion.sql
@@ -0,0 +1,20 @@
+USE master
+
+if exists (select * from sysdatabases where name = 'cine_paraiso')
+  drop database cine_paraiso
+
+CREATE DATABASE cine_paraiso 
+
+USE cine_paraiso
+
+CREATE TABLE ciudad (
+	ciudad_id INTEGER PRIMARY KEY IDENTITY,
+	nombre VARCHAR(50)
+)
+
+CREATE TABLE sucursal (
+	sucursal_id INTEGER PRIMARY KEY IDENTITY,
+	nombre VARCHAR(50),
+	ciudad_id INTEGER FOREIGN KEY REFERENCES ciudad(ciudad_id)
+)
+
