476a32e58b702c94f72887360b72074d0081d2e2
EduardoSegui <107596377+EduardoSegui@users.noreply.github.com>
2022-10-27 19:39:50 -0300

    Update creacion.sql

--- tp1/scripts/creacion.sql
+++ tp1/scripts/creacion.sql
@@ -22,7 +22,7 @@ CREATE TABLE sucursal (
 
 CREATE TABLE sala (
 	sala_id INTEGER PRIMARY KEY IDENTITY,
-	numero INT,
+	numero VARCHAR(2),
 	sucursal_id INTEGER FOREIGN KEY REFERENCES sucursal(sucursal_id)
 )
 
@@ -43,7 +43,7 @@ CREATE TABLE pelicula (
 	pelicula_id INTEGER PRIMARY KEY IDENTITY,
 	nombre VARCHAR(100),
 	subtitulado BIT,
-	clasificacion_id INTEGER
+	clasificacion_id VARCHAR(3)
 )
 
 CREATE TABLE genero (
@@ -57,9 +57,8 @@ CREATE TABLE genero_pelicula (
 )
 
 CREATE TABLE clasificacion (
-	clasificacion_id INTEGER PRIMARY KEY IDENTITY,
-	nombre VARCHAR(50),
-	descripcion VARCHAR(100)
+	clasificacion_id VARCHAR(3) PRIMARY KEY,
+	descripcion VARCHAR(50)
 )
 
 CREATE TABLE compra (
@@ -82,3 +81,87 @@ REFERENCES clasificacion(clasificacion_id)
 ALTER TABLE genero_pelicula 
 ADD CONSTRAINT PK_genero_pelicula
 PRIMARY KEY (pelicula_id, genero_id)
+
+
+INSERT INTO  ciudad (nombre) 
+VALUES ('Rosario')
+
+INSERT INTO  ciudad (nombre) 
+VALUES ('Cordoba')
+
+INSERT INTO  ciudad (nombre) 
+VALUES ('La Plata')
+
+INSERT INTO  sucursal(nombre, ciudad_id) 
+VALUES ('Monumental', 1)
+
+
+INSERT INTO  sucursal(nombre, ciudad_id) 
+VALUES ('EL Pirata', 2)
+
+
+INSERT INTO  sucursal(nombre, ciudad_id) 
+VALUES ('Lobbo', 3)
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (1, '01')
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (1, '02')
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (1, '03')
+
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (2, '01')
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (2, '02')
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (2, '03')
+
+
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (3, '01')
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (3, '02')
+
+INSERT INTO  sala(sucursal_id, numero) 
+VALUES (3, '03')
+
+INSERT INTO  genero(nombre) 
+VALUES ('ciencia ficcion'), ('drama'), ('terror'), ('accion'), ('suspenso') 
+
+INSERT INTO  clasificacion(clasificacion_id) 
+VALUES ('ATP'), ('+13'), ('+16'), ('+18')
+
+
+INSERT INTO  pelicula(nombre, subtitulado, clasificacion_id)
+VALUES  ('Argentina 1985', 0, '+13'), ('2001: Odisea del espacio', 1, '+13'),
+		('Alien', 1, '+18'), ('Gladiador', 1, '+16'), ('Secreto de sus ojos', 0, '+13')    
+
+INSERT INTO genero_pelicula(pelicula_id, genero_id) 
+VALUES (1, 2), (2, 1), (2, 5), (3, 1), (3, 3), (4, 4), (4, 2), (5, 2)   			
+			
+INSERT INTO funcion(fecha_hora, pelicula_id, sala_id) 
+VALUES ('2022-24-10 17:30', 1, 2)
+
+--INSERT INTO funcion(fecha_hora, pelicula_id, sala_id) 
+--VALUES (, 1, 2)
+
+
+--SELECT *
+--FROM funcion
+
+--select GETDATE()
+
+
+select *
+from funcion
+
+
+
