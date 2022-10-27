1d9f7daca9299bfa7da64853e6da0f64b71b5ecc
EduardoSegui <107596377+EduardoSegui@users.noreply.github.com>
2022-10-27 12:31:02 -0300

    agrego tablas

--- tp1/scripts/creacion.sql
+++ tp1/scripts/creacion.sql
@@ -19,4 +19,42 @@ CREATE TABLE sucursal (
 	ciudad_id INTEGER FOREIGN KEY REFERENCES ciudad(ciudad_id)
 )
 
-hola
+CREATE TABLE sala (
+	sala_id INTEGER PRIMARY KEY IDENTITY,
+	numero INT,
+	sucursal_id INTEGER FOREIGN KEY REFERENCES sucursal(sucursal_id)
+)
+
+CREATE TABLE butaca ( 
+	butaca_id INTEGER PRIMARY KEY IDENTITY,
+	posicion VARCHAR(10),
+	sala_id INTEGER FOREIGN KEY REFERENCES sala(sala_id)
+)
+
+CREATE TABLE funcion ( 
+	funcion_id INTEGER PRIMARY KEY IDENTITY,
+	fecha_hora DATETIME,
+	sala_id INTEGER FOREIGN KEY REFERENCES sala(sala_id)
+)
+
+CREATE TABLE pelicula (
+	pelicula_id INTEGER PRIMARY KEY IDENTITY,
+	nombre VARCHAR(100),
+	subtitulado BIT,
+)
+
+CREATE TABLE genero (
+	genero_id INTEGER PRIMARY KEY IDENTITY,
+	nombre VARCHAR(100)
+)
+
+CREATE TABLE genero_pelicula (
+	pelicula_id INTEGER FOREIGN KEY REFERENCES pelicula(pelicula_id),
+	genero_id INTEGER FOREIGN KEY REFERENCES genero(genero_id)
+)
+
+CREATE TABLE clasificacion (
+	clasificacion_id INTEGER PRIMARY KEY IDENTITY,
+	nombre VARCHAR(50),
+	descripcion VARCHAR(100)
+)
