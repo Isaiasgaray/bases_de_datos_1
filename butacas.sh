f528195dc1f72b6d7e1acfb5a46d572db60146ea
Isaías <isaiasgaraygorta@gmail.com>
2022-10-27 23:15:18 -0300

    Modifica tabla y agrega script para generar butacas

---
+++ butacas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+printf "INSERT INTO butaca (posicion, sala_id)\nVALUES"
+
+BUTACAS=$(
+for sala_id in {1..3}; do
+  for letra_pos in {A..E}; do
+    printf "\t"
+    printf "('$letra_pos%s', $sala_id), " {1..5}
+    echo
+  done
+done
+)
+
+echo "${BUTACAS%??}"
--- tp1/scripts/creacion.sql
+++ tp1/scripts/creacion.sql
@@ -28,7 +28,7 @@ CREATE TABLE sala (
 
 CREATE TABLE butaca ( 
 	butaca_id INTEGER PRIMARY KEY IDENTITY,
-	posicion VARCHAR(10),
+	posicion VARCHAR(3),
 	sala_id INTEGER FOREIGN KEY REFERENCES sala(sala_id)
 )
 
@@ -90,41 +90,56 @@ VALUES ('Rosario'),
 	   ('La Plata')
 
 
-INSERT INTO  sucursal(nombre, ciudad_id) 
+INSERT INTO sucursal(nombre, ciudad_id) 
 VALUES ('Monumental', 1),
 	   ('El Pirata', 2),
 	   ('Lobo', 3)
 	 
-INSERT INTO  sala(sucursal_id, numero) 
+INSERT INTO sala (sucursal_id, numero) 
 VALUES (1, '01'), (1, '02'), (1, '03'),
 	   (2, '01'), (2, '02'), (2, '03'),
 	   (3, '01'), (3, '02'), (3, '03')
 
-INSERT INTO  genero(nombre) 
+INSERT INTO genero (nombre) 
 VALUES ('Ciencia ficción'),
 	   ('Drama'),
 	   ('Terror'),
 	   ('Acción'),
 	   ('Suspenso') 
 
-INSERT INTO  clasificacion(nombre) 
+INSERT INTO  clasificacion (nombre) 
 VALUES ('ATP'),
 	   ('+13'),
 	   ('+16'),
 	   ('+18')
 
-INSERT INTO  pelicula(nombre, subtitulado, clasificacion_id)
+INSERT INTO  pelicula (nombre, subtitulado, clasificacion_id)
 VALUES  ('Argentina 1985', 0, 2),
 		('2001: Odisea del espacio', 1, 2),
 		('Alien', 1, 4),
 		('Gladiador', 1, 3),
 		('Secreto de sus ojos', 0, 2)    
 
-INSERT INTO genero_pelicula(pelicula_id, genero_id) 
+INSERT INTO genero_pelicula (pelicula_id, genero_id) 
 VALUES (1, 2), 
 	   (2, 1), (2, 5), 
 	   (3, 1), (3, 3), 
 	   (4, 4), (4, 2),
 	   (5, 2)   			
 
-
+INSERT INTO butaca (posicion, sala_id)
+VALUES	('A1', 1), ('A2', 1), ('A3', 1), ('A4', 1), ('A5', 1), 
+	('B1', 1), ('B2', 1), ('B3', 1), ('B4', 1), ('B5', 1), 
+	('C1', 1), ('C2', 1), ('C3', 1), ('C4', 1), ('C5', 1), 
+	('D1', 1), ('D2', 1), ('D3', 1), ('D4', 1), ('D5', 1), 
+	('E1', 1), ('E2', 1), ('E3', 1), ('E4', 1), ('E5', 1), 
+	('A1', 2), ('A2', 2), ('A3', 2), ('A4', 2), ('A5', 2), 
+	('B1', 2), ('B2', 2), ('B3', 2), ('B4', 2), ('B5', 2), 
+	('C1', 2), ('C2', 2), ('C3', 2), ('C4', 2), ('C5', 2), 
+	('D1', 2), ('D2', 2), ('D3', 2), ('D4', 2), ('D5', 2), 
+	('E1', 2), ('E2', 2), ('E3', 2), ('E4', 2), ('E5', 2), 
+	('A1', 3), ('A2', 3), ('A3', 3), ('A4', 3), ('A5', 3), 
+	('B1', 3), ('B2', 3), ('B3', 3), ('B4', 3), ('B5', 3), 
+	('C1', 3), ('C2', 3), ('C3', 3), ('C4', 3), ('C5', 3), 
+	('D1', 3), ('D2', 3), ('D3', 3), ('D4', 3), ('D5', 3), 
+	('E1', 3), ('E2', 3), ('E3', 3), ('E4', 3), ('E5', 3)
