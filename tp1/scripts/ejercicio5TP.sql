1046c4750dcd201c478d32aa6e0a6b07e4f11f87
EduardoSegui <107596377+EduardoSegui@users.noreply.github.com>
2022-11-02 13:49:01 -0300

    Create ejercicio5TP.sql

---
+++ tp1/scripts/ejercicio5TP.sql
@@ -0,0 +1,61 @@
+USE cine_paraiso
+/*
+5a) El precio de la entrada depende unicamente de la sucursal.
+
+ALTER TABLE sucursal 
+ADD precio FLOAT;
+
+UPDATE sucursal
+
+set precio = 200.00
+where sucursal_id = 1;
+
+UPDATE sucursal
+set precio = 250.44
+where sucursal_id = 2;
+
+UPDATE sucursal
+set precio = 350.44
+where sucursal_id = 3;
+*/
+
+
+-- 5b) El precio de la entrada depende unicamente de la pelicula.
+
+ALTER TABLE pelicula
+ADD precio FLOAT;
+
+UPDATE pelicula
+set precio = 200.00
+where pelicula_id = 1;
+
+UPDATE pelicula
+set precio = 250.44
+where pelicula_id = 2;
+
+UPDATE pelicula
+set precio = 350.44
+where pelicula_id = 3;
+
+UPDATE pelicula
+set precio = 150.44
+where pelicula_id = 4;
+
+UPDATE pelicula
+set precio = 270.44
+where pelicula_id = 5;
+
+/*
+5c) El preico de la entrada depende unicamente de la ubicacion de la butaca.
+ALTER TABLE butaca
+ADD precio FLOAT;
+
+UPDATE butaca
+set precio = 270.44
+where butuca_id = 1;
+
+Para las proximas 19 butacas hacemos el mismo procedimiento cambiando el numero de id de la butaca
+*/
+
+
+
