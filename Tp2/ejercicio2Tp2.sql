1ee3939664c565cb2b5d81ef5c26f15b9c638cc6
Isaías <isaiasgaraygorta@gmail.com>
2022-11-30 18:29:40 -0300

    Corrige tablas

--- Tp2/ejercicio2Tp2.sql
+++ Tp2/ejercicio2Tp2.sql
@@ -1,29 +1,30 @@
 use cine_paraiso
 
-
 CREATE TABLE [plan] (
 	plan_id INTEGER PRIMARY KEY IDENTITY,
 	nombre_plan VARCHAR(50),
 	precio FLOAT,
-	mes_year BIT,
-	
+	mes_year BIT
 )
+
 CREATE TABLE usuario (
 	usuario_id INTEGER PRIMARY KEY IDENTITY,
 	mail VARCHAR(50),
 	usuario VARCHAR(50),
 	pass VARCHAR (50)
 )
-CREATE TABLE pagos (
-	id_pago INTEGER PRIMARY KEY IDENTITY,
+
+CREATE TABLE pago (
+	pago_id INTEGER PRIMARY KEY IDENTITY,
+	usuario_id INTEGER FOREIGN KEY REFERENCES usuario(usuario_id),
 	fecha_pago DATE,
 	precio_pagado FLOAT
 )
 
-CREATE TABLE Subscripcion (
-	id_sub INTEGER PRIMARY KEY IDENTITY,
-	id_usuario INTEGER FOREIGN KEY REFERENCES usuario(usuario_id),
+CREATE TABLE subscripcion (
+	sub_id INTEGER PRIMARY KEY IDENTITY,
+	usuario_id INTEGER FOREIGN KEY REFERENCES usuario(usuario_id),
 	[plan] INT,
-	Vencimiento date
+	vencimiento date
 )
 
